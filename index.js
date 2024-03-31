f2d163f49e0ca58afe37474912d9f7c911722d6a
haraprasad <haraprasadchatterjee7@gmail.com>
2024-03-31 10:01:08 +0530

    port change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
+const PORT = process.env.PORT || 8080; // Use the port provided by the environment or default to 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
