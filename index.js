30e73a75c6af0ea2ddf2716511ed9aac451fdb71
haraprasad <haraprasadchatterjee7@gmail.com>
2024-03-31 09:58:07 +0530

    inital set up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, 'dist', 'pmgui')));
+
+// Serve the Angular app's index.html for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist'  ,'pmgui', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
