39f871f2d3918fd2a5d3d8548565d4a476f1521d
haraprasad <haraprasadchatterjee7@gmail.com>
2024-04-11 14:52:32 +0530

    certificate

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
+const httpServer = require("https").createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'private.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'certificate.crt')),
+  ca:fs.readFileSync(path.join(__dirname, 'cert', 'ca_bundle.crt'))
+},app);
 
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist', 'pmgui')));
@@ -13,6 +18,6 @@ app.get('*', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 8080; // Use the port provided by the environment or default to 3000
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
